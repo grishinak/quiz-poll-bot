0ed3d646f3c92c791f3c8ed001eecc9d6448f348
grishinak <st122261@student.spbu.ru>
2024-11-17 20:50:14 +0300

    Add: update user_last&first_name if they existed

--- tgbot/database/requests.py
+++ tgbot/database/requests.py
@@ -1,12 +1,21 @@
 from database.models import async_session
-from database.models import User, Poll, Player, Answer
-from sqlalchemy import select  # , update, delete
+from database.models import User
+from sqlalchemy import select
 
 
 async def set_user(tg_id, first_name, last_name):
     async with async_session() as session:
+        # Получаем пользователя из базы данных
         user = await session.scalar(select(User).where(User.tg_id == tg_id))
 
-    if not user:
-        session.add(User(tg_id=tg_id, first_name=first_name, last_name=last_name))
+        if not user:
+            # Если пользователь отсутствует, добавляем нового
+            user = User(tg_id=tg_id, first_name=first_name, last_name=last_name)
+            session.add(user)
+        else:
+            # Если пользователь существует, обновляем его данные
+            user.first_name = first_name
+            user.last_name = last_name
+
+        # Фиксируем изменения в базе данных
         await session.commit()
