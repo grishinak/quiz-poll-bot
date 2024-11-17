688c6acccd6f0bde71343937c3acac07859f9d2b
grishinak <st122261@student.spbu.ru>
2024-11-17 19:53:19 +0300

    Add SQLite3 db

---
+++ tgbot/database/models.py
@@ -0,0 +1,53 @@
+from sqlalchemy import BigInteger, String, ForeignKey
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
+
+engine = create_async_engine(url="sqlite+aiosqlite:///db.sqlite3")
+
+async_session = async_sessionmaker(engine)
+
+# base class
+class Base(AsyncAttrs, DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    tg_id = mapped_column(BigInteger)
+    first_name: Mapped[str] = mapped_column(String(50))
+    last_name: Mapped[str] = mapped_column(String(50))
+
+
+class Poll(Base):
+    __tablename__ = "polls"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50))
+    question: Mapped[str] = mapped_column(String(250))
+    answer: Mapped[str] = mapped_column(String(250))
+    creator_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+
+
+class Player(Base):
+    __tablename__ = "players"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    poll_id: Mapped[int] = mapped_column(ForeignKey("polls.id"))
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+
+
+class Answer(Base):
+    __tablename__ = "answers"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    poll_id: Mapped[int] = mapped_column(ForeignKey("polls.id"))
+    player_id: Mapped[int] = mapped_column(ForeignKey("players.id"))
+    answer: Mapped[str] = mapped_column(String(250))
+
+
+#
+async def async_main():
+    async with engine.begin() as connection:
+        await connection.run_sync(Base.metadata.create_all)
--- tgbot/main.py
+++ tgbot/main.py
@@ -10,8 +10,12 @@ from aiogram import Bot, Dispatcher
 from handlers.handler import router as handler_router
 from handlers.create_poll import router as create_poll_router
 
+# db
+from database.models import async_main
+
 
 async def main():
+    await async_main()  # create db
     load_dotenv()
     bot = Bot(token=os.getenv("BOT_TOKEN"))
     dp = Dispatcher()
