c4e1de4b692ed5da3a279095aedfc48d560beca1
grishinak <st122261@student.spbu.ru>
2024-11-19 23:23:58 +0300

    Add /create_poll to help list

--- tgbot/handlers/help.py
+++ tgbot/handlers/help.py
@@ -3,7 +3,7 @@ from aiogram.filters import Command
 from aiogram.types import Message
 
 router = Router()
-help_text = """Список команд:\n/start - инициализация бота.\n/help - получение информации о боте.\n"""
+help_text = """Список команд:\n/start - инициализация бота.\n/help - получение информации о боте.\n /create_poll - создание опроса"""
 
 # /help handler
 @router.message(Command("help"))
