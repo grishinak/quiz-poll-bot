8a3f5917c90053f3feb7f405e49d44571f7602d0
grishinak <st122261@student.spbu.ru>
2024-11-28 22:12:45 +0300

    Change /connect_lobby to FSM instead of/... <id>

--- tgbot/handlers/connect_lobby.py
+++ tgbot/handlers/connect_lobby.py
@@ -3,19 +3,37 @@ from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
 from aiogram.filters import Command
 from aiogram import Bot
+from aiogram.fsm.state import State, StatesGroup  # Импортируем StatesGroup
 
 import database.requests as rq
 
-# import keyboards.connect_lobby as kb
+# Определяем состояние для FSM с использованием StatesGroup
+class LobbyState(StatesGroup):  # Наследуем от StatesGroup
+    waiting_for_lobby_id = State()  # Ожидаем ID лобби
+
 
 router = Router()
 
 # Обработчик команды /connect_lobby
 @router.message(Command("connect_lobby"))
-async def cmd_connect_lobby(message: Message, state: FSMContext, bot: Bot):
-    # Проверим, если пользователь создатель лобби
-    lobby_id = message.text.split()[1]  # Пример: /connect_lobby 1
-    lobby_id = int(lobby_id)
+async def cmd_connect_lobby(message: Message, state: FSMContext):
+    # Запрашиваем ID лобби
+    await message.answer("К какому лобби вы хотите подключиться?")
+    await state.set_state(
+        LobbyState.waiting_for_lobby_id
+    )  # Переход к состоянию ожидания ID лобби
+
+
+# Обработчик для получения ID лобби и подключения пользователя
+@router.message(LobbyState.waiting_for_lobby_id)
+async def process_lobby_id(message: Message, state: FSMContext, bot: Bot):
+    try:
+        # Преобразуем введенный текст в ID лобби
+        lobby_id = int(message.text)
+    except ValueError:
+        # Если ID не является числом
+        await message.answer("Пожалуйста, введите действительный ID лобби.")
+        return
 
     # Проверим существование лобби
     if not await rq.check_lobby_exists(lobby_id):
@@ -27,14 +45,15 @@ async def cmd_connect_lobby(message: Message, state: FSMContext, bot: Bot):
         await message.answer("Вы уже в этом лобби!")
         return
 
+    # Подключаем пользователя к лобби
     await rq.set_lobby_participant(lobby_id, message.from_user.id)
     await message.answer(f"Вы подключились к лобби {lobby_id}!")
 
-    # Получим poll_id и сам вопрос
+    # Получаем poll_id и сам вопрос
     poll_id = await rq.get_poll_id_for_lobby(lobby_id)
     question = await rq.get_poll_question(poll_id)
 
-    # Получим всех участников лобби
+    # Получаем всех участников лобби
     participants = await rq.get_lobby_participants(lobby_id)
 
     # Отправим вопрос всем участникам
@@ -42,4 +61,7 @@ async def cmd_connect_lobby(message: Message, state: FSMContext, bot: Bot):
         # отправим вопрос каждому участнику лобби
         await bot.send_message(
             participant_id, f"Вопрос: {question}"
-        )  # сейчас вопрос сразу при подключении к лобби отправлен
+        )  # сейчас сразу при подключении отправляется всем
+
+    # Завершаем сессию
+    await state.clear()  # Завершаем FSM после успешного подключения
